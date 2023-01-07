99b3bf0b6f1beb04505479fb6662b7594de03152
irineu silva <mmmirineusilva@gmail.com>
2023-01-07 18:18:03 -0300

    Initial commit: empty project

---
+++ app/src/main/java/com/neo/hash/ui/theme/Theme.kt
@@ -0,0 +1,53 @@
+package com.neo.hash.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+@Composable
+fun HashTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
+
+@Composable
+fun HashBackground(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colors.background,
+    content: @Composable () -> Unit
+) = Surface(
+    modifier = modifier,
+    color = color,
+    content = content
+)
